c2375dba9465eef8c83362a68802568e41ff75d8
Robert Cheung <robert.cheung@venmo.com>
2014-04-26 13:53:46 -0700

    generify build script

--- build_app.sh
+++ build_app.sh
@@ -1,7 +1,7 @@
-rm -rf "/Users/jaychae/Desktop/Sweeper.app"
+rm -f "~/Desktop/Sweeper.app"
 
-xcodebuild -archivePath /Users/jaychae/Desktop/Sweeper -scheme Sweeper -configuration Release archive 
-xcodebuild -exportArchive -exportFormat APP -archivePath /Users/jaychae/Desktop/Sweeper.xcarchive -exportPath /Users/jaychae/Desktop/Sweeper
+xcodebuild -archivePath ~/Desktop/Sweeper -scheme Sweeper -configuration Release archive 
+xcodebuild -exportArchive -exportFormat APP -archivePath ~/Desktop/Sweeper.xcarchive -exportPath ~/Desktop/Sweeper
 
 rm -rf /Applications/Sweeper.app
-mv /Users/jaychae/Desktop/Sweeper.app /Applications/Sweeper.app
+mv ~/Sweeper.app /Applications/Sweeper.app
